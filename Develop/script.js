5f7de6574400287e8ba611b62beb302bc930ddf2
buildgal <gjaquelin7@outlook.com>
2020-11-01 16:03:34 -0600

    removing all the comments that are not needed

--- Develop/script.js
+++ Develop/script.js
@@ -9,11 +9,7 @@ let momentTime=moment().format('LT');
 let arrMomentTime= momentTime.split(":");
 let timeSlot= ["9","10","11","12","1","2","3","4","5"];
 let currentHour= arrMomentTime[0];
-//let a= timeSlot[5];
-//let c= $("#"+"5");
-//if (a===currentHour){
-  //  c.css("background-color", "yellow");
-//}
+
 
 for (let i=0; i<timeSlot.length; i++){
     if (timeSlot[i]===currentHour){
@@ -21,29 +17,20 @@ for (let i=0; i<timeSlot.length; i++){
         inputField.css("background-color", "yellow");
     }
 
-}
+    else if (timeSlot[i]>currentHour ){
+        let inputField=$("#"+i);
+        inputField.css("background-color", "green");
+        let NineAm=$("#0");
+        NineAm.css("background-color","orange");
+    }
 
-
-/*
-
-if (timeSlot===currentHour){
-    le
-
-    $("input-group-text").css("background-color", "yellow");
-} 
-    
-else{
-    alert("dont do anything");
+    else if (timeSlot[i]<currentHour){
+        let inputField=$("#"+i);
+        inputField.css("background-color", "orange");
+    }
 
 }
-*/
-    
-/*
-current time 1pm- 9-12: grey and 2-5: green 
-curr onrent time 11am- 12-5 green and 9-10 grey
-crent time 3pm- 
-Q: What how do you change the color 
-*/
+
 
 //Local Storage: when you update something on the form and refresh the page it saves 
 //need an onclick function 
