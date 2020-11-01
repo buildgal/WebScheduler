b68c52204b4fc8484185b0f3b62dc091730d705d
buildgal <gjaquelin7@outlook.com>
2020-11-01 14:47:04 -0600

    matching the current time to the field on the screen

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,44 @@
 //sets the current date at the top of the page 
 let currentDate= $("#currentDay");
-let currentDateFromat=moment().format("MMM Do YY");
+let currentDateFromat=moment().format('MMMM Do YYYY, h:mm:ss a')
 currentDate.text(currentDateFromat);
 
 
 //comparing the time tag inside each textbox to the current time 
-let workDayTime= $(this).attr("src",$(this).attr("time"));
 let momentTime=moment().format('LT');
-let arrMomentTime= momentTime.split();
+let arrMomentTime= momentTime.split(":");
+let timeSlot= ["9","10","11","12","1","2","3","4","5"];
+let currentHour= arrMomentTime[0];
+//let a= timeSlot[5];
+//let c= $("#"+"5");
+//if (a===currentHour){
+  //  c.css("background-color", "yellow");
+//}
 
-if (workDayTime>arrMomentTime){ 
-    //need to change the color of the box 
-    alert("displays if current time"+ arrMomentTime)
+for (i=0; timeSlot[i]<timeSlot.length; i++){
+    let plannerTime=timeSlot[i];
+    let inputField= $("#"+i);
+    console.log(plannerTime);
+    console.log(inputField);
+    if (plannerTime===currentHour){
+        inputField.css("background-color", "yellow");
+        
+    }
 }
+
+/*
+
+if (timeSlot===currentHour){
+    le
+
+    $("input-group-text").css("background-color", "yellow");
+} 
+    
 else{
     alert("dont do anything");
 
 }
+*/
     
 /*
 current time 1pm- 9-12: grey and 2-5: green 
@@ -24,3 +46,8 @@ curr onrent time 11am- 12-5 green and 9-10 grey
 crent time 3pm- 
 Q: What how do you change the color 
 */
+
+//Local Storage: when you update something on the form and refresh the page it saves 
+//need an onclick function 
+//key and value from the local stroage 
+//display the local storage 
